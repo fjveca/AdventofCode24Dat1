fa7035cbeb0f5e2e0ea2077ab9ae3693a6e0f07f
Francisco Velo <fjveca@gmail.com>
2024-12-01 13:13:38 -0400

    final answer to first advent of code for day 1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fjveca/AdventofCode24Dat1
+
+go 1.22.1
+
+require golang.org/x/tools v0.27.0
--- main.go
+++ main.go
@@ -2,18 +2,56 @@ package main
 
 import (
 	"fmt"
-	"buffio"
-	"errors"
+	"log"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 func main() {
 
-  input, error := os.OpenFile("input", os.O_RDONLY)
-  if error != nil {
-	log.fatal("no valid input file found")
-  }
-  
+	var values1, values2, distance []int
+	var TotalDistance, substraction int
+	content, err := os.ReadFile("input")
+	if err != nil {
+		log.Fatal("no valid input file found")
+	}
 
+	sets := strings.Split(string(content), "\r\n")
+	for _, set := range sets {
+		items := strings.Split(set, "   ")
+		buffer, _ := strconv.Atoi(items[0])
+		values1 = append(values1, buffer)
+		buffer, _ = strconv.Atoi(items[1])
+		values2 = append(values2, buffer)
+	}
+	sort.Slice(values1, func(i, j int) bool {
+		return values1[i] < values1[j]
+	})
+	sort.Slice(values2, func(i, j int) bool {
+		return values2[i] < values2[j]
+	})
+
+	for i := 0; i < len(values1); i++ {
+		//fmt.Printf("total distance on point %v: %v ", i, TotalDistance)
+		if values1[i] < values2[i] {
+			//distance = append(distance, values2[i]-values1[i])
+			substraction = values2[i] - values1[i]
+			//println("value 2 is higher")
+
+		} else {
+			//distance = append(distance, values1[i]-values2[i])
+			substraction = values1[i] - values2[i]
+			//println("value 1 is higher")
+		}
+		distance = append(distance, substraction)
+		TotalDistance += substraction
+	}
+	println("total distance between lists:", TotalDistance)
+	verify := 0
+	for _, separation := range distance {
+		verify += separation
+	}
+	fmt.Println(verify, "=", TotalDistance)
 }
